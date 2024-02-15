198628ffe37448b639eef33f672355721400dafa
June-Senpai <keshav01juneja@gmail.com>
2024-02-16 02:13:43 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+  console.log(import.meta.env.VITE_WEATHER_API)
+
+  return (
+    <main>
+      <div className="bg-red-500">hi</div>
+    </main>
+  )
+}
+
+export default App
