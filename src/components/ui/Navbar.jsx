18c6653ee95eac0e55af42d8aedf64982c9a7434
June-Senpai <keshav01juneja@gmail.com>
2024-02-18 03:09:49 +0530

    style: navbar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,8 +1,17 @@
 const Navbar = () => {
   return (
     <header>
-      <nav>
-        <div className="text-2xl">Weather Forecast</div>
+      <nav className="relative ml-2">
+        <div className="ml-14 mt-2 p-4 text-lg md:text-2xl">
+          Weather Forecast
+        </div>
+        <img
+          src="/weather-icon-png-2.png"
+          alt="company icon"
+          width="62px"
+          height="62px"
+          className="absolute top-2"
+        />
       </nav>
     </header>
   );
