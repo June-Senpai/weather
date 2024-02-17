6153f02d84263b2688012768d2a22252047e9bd8
June-Senpai <keshav01juneja@gmail.com>
2024-02-18 02:47:15 +0530

    style: nav and installing tailwind prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState, useEffect, useRef } from "react"
-import axios from "axios"
-import "./App.css"
-import Card from "./components/Card"
-import Navbar from "./components/ui/Navbar"
-import Footer from "./components/ui/Footer"
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import "./App.css";
+import Card from "./components/Card";
+import Navbar from "./components/ui/Navbar";
+import Footer from "./components/ui/Footer";
 
 function App() {
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState("")
-  const inputRef = useRef(null)
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const inputRef = useRef(null);
 
   const handleSearch = () => {
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&limit=5&appid=${
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${
       import.meta.env.VITE_WEATHER_API
-    }`
+    }`;
     axios
       .get(url)
       .then((res) => {
@@ -25,29 +25,35 @@ function App() {
         // console.log("city:", city) // Object containing city information
         // console.log("city name:", city.name) // London
         // console.log("city population:", city.population) // 1000000
-        setData(res.data)
-        console.log(res.data)
+        setData(res.data);
+        console.log(res.data);
       })
       .catch((error) => {
-        console.error("Error fetching weather data:", error)
-      })
-  }
+        console.error("Error fetching weather data:", error);
+      });
+  };
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
-      e.preventDefault()
-      handleSearch()
+      e.preventDefault();
+      handleSearch();
     }
-  }
+  };
 
   useEffect(() => {
-    inputRef.current.focus()
-  }, [])
+    inputRef.current.focus();
+  }, []);
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="mx-auto max-w-7xl">
       <Navbar />
       <main className="min-h-screen">
-        <form onSubmit={handleSearch} className="flex justify-center gap-2">
+        <h2 className="mx-auto w-1/3 text-center text-4xl">
+          Discover the weather in every city you go
+        </h2>
+        <form
+          onSubmit={handleSearch}
+          className="flex items-center justify-center gap-2"
+        >
           <input
             type="text"
             value={location}
@@ -55,15 +61,17 @@ function App() {
             placeholder="Enter location"
             onKeyDown={handleKeyPress}
             ref={inputRef}
-            className=" w-72 bg-slate-500 rounded-lg "
+            className=" h-12 w-72 rounded-lg bg-slate-500"
           />
-          <button className="bg-green-500 text-white px-4 py-2 mt-2 rounded-md">Search</button>
+          <button className=" rounded-lg bg-green-500 p-3 text-white">
+            Search
+          </button>
         </form>
         {!!Object.keys(data).length > 0 && <Card data={data} />}
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -2,9 +2,9 @@ const Navbar = () => {
   return (
     <header>
       <nav>
-        <h1>Weather Forecast</h1>
+        <div className="text-2xl">Weather Forecast</div>
       </nav>
     </header>
-  )
-}
-export default Navbar
+  );
+};
+export default Navbar;
